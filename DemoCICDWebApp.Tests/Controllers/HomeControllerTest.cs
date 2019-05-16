c9e0502ec59a8b996b47fe66406652dd38d5a526
atanu.sarkar <atanu.sarkar@DESKTOP-OFLMFQK>
2019-05-16 16:19:58 +0530

    Add project files.

---
+++ DemoCICDWebApp.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoCICDWebApp;
+using DemoCICDWebApp.Controllers;
+
+namespace DemoCICDWebApp.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
